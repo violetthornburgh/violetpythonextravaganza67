615a65dd94d7a1e3d1e67e902ba486c883f74233
violetthornburgh <v80621331@gmail.com>
2026-04-04 12:24:47 -0700

    Create whileloop.py
    you are only allowed to be cole widdowson after you have been eli

---
+++ whileloop.py
@@ -0,0 +1,14 @@
+
+name = input("what's your name")
+
+
+
+while (name == "cole widdowson"):
+  print("are you sure?")
+  name = input("what's your name")
+
+while (name == "eli"):
+  print("not it's not.")
+  name = input("what is it really")
+
+  
